4f232b8918bcbc5fa4a0b3b9ea452a65b2412e09
Bambjerg <44673239+Bambjerg@users.noreply.github.com>
2020-04-22 09:57:47 +0200

    light.h added

---
+++ RTOS_TEMPLATE-master/PlanteModul.cydsn/light.h
@@ -0,0 +1,34 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+/* [] END OF FILE */
+
+#ifndef NUTRIENTS_H 
+    #define NUTRIENTS_H 
+    #define NUTRIENTSTEST 1 //set this to 0 to disable Debug/test
+    
+    
+    
+    
+    #if NUTRIENTSTEST == 1
+        /* --- TEST TASK/FUNCTIONS/VARIABLES --- */
+        
+        /*
+        Task for testing existing implementations.
+
+        DO NOT delete this functions/tasks, but comment them out if they arn't nexessary 
+        for your current implementation/testing
+    
+        All test task/functions, must start with (type)Test(Task/Function/Var)(Name)
+        */
+    #endif
+#endif
